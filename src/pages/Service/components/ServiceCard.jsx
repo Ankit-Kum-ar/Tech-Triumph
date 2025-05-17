42bd5210bac8a4bbc484a5475f0ca74323ef4807
Sahil Rawat <sahilr8587@gmail.com>
2025-05-17 21:29:24 +0530

    Refactor ServiceCard component: Adjust layout proportions and enhance image hover effect

--- src/pages/Service/components/ServiceCard.jsx
+++ src/pages/Service/components/ServiceCard.jsx
@@ -5,18 +5,18 @@ const ServiceCard = ({ title, description, staticSrc, index }) => {
     <div
     className={`flex flex-col max-w-full md:flex-row ${
         index % 2 === 0 ? "md:flex-row-reverse" : ""
-      } items-center gap-6 mb-12`}
+      } items-center gap-6 mb-6`}
     >
-      <div className="w-full max-w-7xl md:w-1/2 flex justify-center">
+      <div className="w-full max-w-7xl md:w-1/3 flex justify-center">
         <img
           src={staticSrc}
           alt={title}
-          className="h-72 object-contain"
+          className="h-72 object-contain hover:scale-105 transition-transform duration-300 ease-in-out"
         />
       </div>
-      <div className="w-full md:w-1/2  px-4">
+      <div className="w-full md:w-2/3 px-8">
         <h2 className="text-xl md:text-4xl font-semibold mb-2">{title}</h2>
-        <p className="text-sm md:text-base text-justify">{description}</p>
+        <p className="text-sm md:text-sm text-justify">{description}</p>
       </div>
     </div>
   );
