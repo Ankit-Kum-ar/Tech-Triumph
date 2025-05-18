42b6948431e540d9a5386b5689808aa0515d9e44
Sahil Rawat <sahilr8587@gmail.com>
2025-05-18 12:00:15 +0530

    Refactor Service component: Consolidate imports for Hero and ServiceCard components

--- src/pages/Service/Service.jsx
+++ src/pages/Service/Service.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import Hero from './components/Hero'
-import ServiceCard from './components/ServiceCard';
+
 import { serviceprovided } from '../../constant';
+import { Hero, ServiceCard } from './components';
 
 const Service = () => {
 
